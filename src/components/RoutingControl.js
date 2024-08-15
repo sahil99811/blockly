cb3fac42149b6cc3cf035fff7f445d3a41f1d4d5
sahil99811 <sp220253@gmail.com>
2024-08-15 08:43:59 +0530

    testing code

--- src/components/RoutingControl.js
+++ src/components/RoutingControl.js
@@ -1,40 +1,44 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useMap } from 'react-leaflet';
 import L from 'leaflet';
-import 'leaflet-routing-machine'; 
+import 'leaflet-routing-machine';
+
 const RoutingControl = ({ start, destination }) => {
   const map = useMap();
+  const routingControlRef = useRef(null);
 
   useEffect(() => {
     if (!map || !start || !destination) return;
 
-    const routingControl = L.Routing.control({
-      waypoints: [
+    if (routingControlRef.current) {
+      routingControlRef.current.setWaypoints([
         L.latLng(start[0], start[1]),
         L.latLng(destination[0], destination[1]),
-      ],
-      routeWhileDragging: true,
-      lineOptions: {
-        styles: [{ color: 'green', weight: 3 }],
-      },
-      createMarker: () => null, 
-      addWaypoints: false,
-      draggableWaypoints: false, 
-    }).addTo(map);
-
-   
- 
- const controlContainer = document.querySelector('.leaflet-routing-container');
-    if (controlContainer) {
-       controlContainer.style.display = 'none';
+      ]);
+    } else {
+      routingControlRef.current = L.Routing.control({
+        waypoints: [
+          L.latLng(start[0], start[1]),
+          L.latLng(destination[0], destination[1]),
+        ],
+        routeWhileDragging: true,
+        lineOptions: {
+          styles: [{ color: 'green', weight: 3 }],
+        },
+        createMarker: () => null, 
+        addWaypoints: false,
+        draggableWaypoints: false,
+      }).addTo(map);
     }
 
+    const controlContainer = document.querySelector('.leaflet-routing-container');
+    if (controlContainer) {
+      controlContainer.style.display = 'none';
+    }
 
-    return () => map.removeControl(routingControl);
   }, [map, start, destination]);
 
   return null;
 };
 
 export default RoutingControl;
-
